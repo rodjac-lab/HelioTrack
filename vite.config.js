30ca217efecb6dc8fc9950923b4b7066438de106
Rodolphe Jacson <rodolphe.jacson@gmail.com>
2025-10-25 23:20:41 +0200

    build: isolate three chunk

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+﻿import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 800,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ["three"],
+        },
+      },
+    },
+  },
+});
